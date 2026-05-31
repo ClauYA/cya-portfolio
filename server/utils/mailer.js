b1a12cbd0538e1f828c9d79cee7ae6fc0952dbbc
ClauYA <cyazoe5@gmail.com>
2026-05-31 11:19:25 -0400

    backup to port 465

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,13 +1,13 @@
+const nodemailer = require('nodemailer');
+
 const transporter = nodemailer.createTransport({
   host: 'smtp.hostinger.com',
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false
-  }
 });
+
+module.exports = transporter;
